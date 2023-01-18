5f10182ca166d116596fe400854c26354192c76d
Baz <61154071+bazmurphy@users.noreply.github.com>
2023-01-18 09:37:01 +0000

    water section five

--- 07-combined-preview/03-water/water.js
+++ 07-combined-preview/03-water/water.js
@@ -1,28 +1,28 @@
 import { waterComponents } from './../05-javascript/waterComponents.js';
-import { 
-  waterSectionOneCollapsibleTarget, 
-  waterSectionOneCollapsibleSubcontainers,
-  waterSectionTwoCollapsibleTarget, 
-  waterSectionTwoCollapsibleSubcontainers,
-  waterSectionThreeCollapsibleTarget, 
-  waterSectionThreeCollapsibleSubcontainers,
-  waterSectionFiveCollapsibleTarget,
-  waterSectionFiveCollapsibleSubcontainers,
-  waterSectionSixCollapsibleTarget,
-  waterSectionSixCollapsibleSubcontainers
-  } from '../05-javascript/waterCollapsibles.js';
+// import { 
+//   waterSectionOneCollapsibleTarget, 
+//   waterSectionOneCollapsibleSubcontainers,
+//   waterSectionTwoCollapsibleTarget, 
+//   waterSectionTwoCollapsibleSubcontainers,
+//   waterSectionThreeCollapsibleTarget, 
+//   waterSectionThreeCollapsibleSubcontainers,
+//   waterSectionFiveCollapsibleTarget,
+//   waterSectionFiveCollapsibleSubcontainers,
+//   waterSectionSixCollapsibleTarget,
+//   waterSectionSixCollapsibleSubcontainers
+//   } from '../05-javascript/waterCollapsibles.js';
 import { fetchAndAddComponentsToDom } from './../05-javascript/fetchAndAddComponentsToDom.js';
-import { setupCollapsibleSubcontainers } from './../05-javascript/setupCollapsibleSubcontainers.js';
+// import { setupCollapsibleSubcontainers } from './../05-javascript/setupCollapsibleSubcontainers.js';
 
 console.log(waterComponents);
 
 async function initialise(){
     await fetchAndAddComponentsToDom(waterComponents);
-    setupCollapsibleSubcontainers(waterSectionOneCollapsibleTarget, waterSectionOneCollapsibleSubcontainers);
-    setupCollapsibleSubcontainers(waterSectionTwoCollapsibleTarget, waterSectionTwoCollapsibleSubcontainers);
-    setupCollapsibleSubcontainers(waterSectionThreeCollapsibleTarget, waterSectionThreeCollapsibleSubcontainers);
-    setupCollapsibleSubcontainers(waterSectionFiveCollapsibleTarget, waterSectionFiveCollapsibleSubcontainers);
-    setupCollapsibleSubcontainers(waterSectionSixCollapsibleTarget, waterSectionSixCollapsibleSubcontainers);
+    // setupCollapsibleSubcontainers(waterSectionOneCollapsibleTarget, waterSectionOneCollapsibleSubcontainers);
+    // setupCollapsibleSubcontainers(waterSectionTwoCollapsibleTarget, waterSectionTwoCollapsibleSubcontainers);
+    // setupCollapsibleSubcontainers(waterSectionThreeCollapsibleTarget, waterSectionThreeCollapsibleSubcontainers);
+    // setupCollapsibleSubcontainers(waterSectionFiveCollapsibleTarget, waterSectionFiveCollapsibleSubcontainers);
+    // setupCollapsibleSubcontainers(waterSectionSixCollapsibleTarget, waterSectionSixCollapsibleSubcontainers);
 }
 
 initialise();
