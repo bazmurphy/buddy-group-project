7c94b9aab6f8fd73bb536a0f3870474319adbf12
Baz <61154071+bazmurphy@users.noreply.github.com>
2023-01-26 18:14:07 +0000

    combined preview adjustments

--- 07-combined-preview/03-water/water.js
+++ 07-combined-preview/03-water/water.js
@@ -1,27 +1,9 @@
 import { waterComponents } from './../05-javascript/waterComponents.js';
-// import { 
-//   waterSectionOneCollapsibleTarget, 
-//   waterSectionOneCollapsibleSubcontainers,
-//   waterSectionTwoCollapsibleTarget, 
-//   waterSectionTwoCollapsibleSubcontainers,
-//   waterSectionThreeCollapsibleTarget, 
-//   waterSectionThreeCollapsibleSubcontainers,
-//   waterSectionFiveCollapsibleTarget,
-//   waterSectionFiveCollapsibleSubcontainers,
-//   waterSectionSixCollapsibleTarget,
-//   waterSectionSixCollapsibleSubcontainers
-//   } from '../05-javascript/waterCollapsibles.js';
 import { fetchAndAddComponentsToDom } from './../05-javascript/fetchAndAddComponentsToDom.js';
-// import { setupCollapsibleSubcontainers } from './../05-javascript/setupCollapsibleSubcontainers.js';
 import { addMobileNavigationEventListeners } from "./../05-javascript/headerMobileNavigation.js";
 
 async function initialise(){
   await fetchAndAddComponentsToDom(waterComponents);
-  // setupCollapsibleSubcontainers(waterSectionOneCollapsibleTarget, waterSectionOneCollapsibleSubcontainers);
-  // setupCollapsibleSubcontainers(waterSectionTwoCollapsibleTarget, waterSectionTwoCollapsibleSubcontainers);
-  // setupCollapsibleSubcontainers(waterSectionThreeCollapsibleTarget, waterSectionThreeCollapsibleSubcontainers);
-  // setupCollapsibleSubcontainers(waterSectionFiveCollapsibleTarget, waterSectionFiveCollapsibleSubcontainers);
-  // setupCollapsibleSubcontainers(waterSectionSixCollapsibleTarget, waterSectionSixCollapsibleSubcontainers);
   addMobileNavigationEventListeners();
 }
 
